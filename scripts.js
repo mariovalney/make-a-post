c7eb11bede3a46f1cbd9cee3e301d2e0905653de
Mário Valney <mario@vizir.com.br>
2018-01-18 15:50:00 -0300

    [Feat] Ace Editor in textarea and better parsing JSON

--- scripts.js
+++ scripts.js
@@ -4,16 +4,23 @@ jQuery.support.cors = true;
 
 jQuery(document).ready(function($) {
     var url = Cookies.get('make_a_post_url'),
-        data = Cookies.get('make_a_post_data');
+        data = Cookies.get('make_a_post_data'),
+        editor = ace.edit( 'api_body_editor' );
 
-    if (url) {
-        $('#api_url').val(url);
-    }
+    // Cookies
+    if (url) $('#api_url').val(url);
+    if (data) $('#api_body').val(data);
 
-    if (data) {
-        $('#api_body').val(data);
-    }
+    // Ace Editor
+    $('#api_body').hide();
+    editor.setShowPrintMargin(false);
+    editor.getSession().setMode( 'ace/mode/json' );
+    editor.getSession().setValue($('#api_body').val());
+    editor.getSession().on('change', function() {
+        $('#api_body').val(editor.getSession().getValue());
+    });
 
+    // Request
     $('#api_post').on('submit', function(event) {
         event.preventDefault();
 
@@ -22,18 +29,30 @@ jQuery(document).ready(function($) {
         var url = $('#api_url').val(),
             data = $('#api_body').val();
 
-        $(this).addClass('submiting');
-        $('#status').text('Submiting...');
+        if (!data) {
+            alert( 'You cannot submit a empty JSON' );
+            return false;
+        }
 
+        var json_data = '';
+        try {
+            json_data = data.replace( /(\r\n|\n|\r)/gm, '' );
+            json_data = JSON.parse(json_data);
+        } catch(e) {
+            alert( 'The data is not a valid JSON.' );
+            return false;
+        }
+
+        // Saving Cookies
         Cookies.set('make_a_post_url', url);
         Cookies.set('make_a_post_data', data);
 
-        data = data.replace( /(\r\n|\n|\r)/gm, '' );
-        data = JSON.parse(data);
+        $(this).addClass('submiting');
+        $('#status').text('Submiting...');
 
         $.ajax({
             url: url,
-            data: data,
+            data: json_data,
             type: 'POST',
             dataType: 'JSON',
             crossDomain: true,
